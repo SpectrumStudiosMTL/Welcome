bdf0cf961cd389f8545f4a89ba0aad2a0eb5e9e3
SpectrumStudiosMTL <cameron@productionsspectrum.com>
2026-06-22 11:47:51 -0400

    Upload

---
+++ data.js
@@ -0,0 +1,311 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7510617117076297,
+        "pitch": 0.013427803706811403,
+        "fov": 1.3331398962253913
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.748359622259013,
+          "pitch": 0.12146556730217206,
+          "rotation": 0,
+          "target": "1-main-room"
+        },
+        {
+          "yaw": 2.71457013647124,
+          "pitch": 0.027675368167567882,
+          "rotation": 0,
+          "target": "2-greenscreen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.478936984644913,
+          "pitch": -0.07475184616812669,
+          "title": "Stop Animation Top View",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.725791285053436,
+          "pitch": 0.18147934384091968,
+          "title": "Filtered Water",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.504391684185128,
+          "pitch": 0.14051446033215953,
+          "title": "Clothes and Bag Storage",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.812355510154763,
+          "pitch": 0.1434988044202079,
+          "title": "EXIT",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3263854855884425,
+          "pitch": 0.0642029570283924,
+          "title": "Coffee Machine",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-main-room",
+      "name": "Main Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.24592252170036488,
+        "pitch": 0.10793888364325177,
+        "fov": 1.3331398962253913
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.848068064908361,
+          "pitch": 0.09310920834926506,
+          "rotation": 0,
+          "target": "3-edit-suite"
+        },
+        {
+          "yaw": 2.4352609609783133,
+          "pitch": 0.13159270175948912,
+          "rotation": 0,
+          "target": "2-greenscreen"
+        },
+        {
+          "yaw": -2.736028265528514,
+          "pitch": 0.11555282888290286,
+          "rotation": 0,
+          "target": "0-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9129665267495497,
+          "pitch": 0.09455967133072463,
+          "title": "Stop Animation Booth",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.5702576788083604,
+          "pitch": 0.19993976985342776,
+          "title": "Editing and Animation Station",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-greenscreen",
+      "name": "GreenScreen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.027304508208594314,
+        "pitch": 0.19244174562947336,
+        "fov": 1.3331398962253913
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.056156555410169,
+          "pitch": 0.05078907891473783,
+          "rotation": 0,
+          "target": "1-main-room"
+        },
+        {
+          "yaw": 2.8446260624343536,
+          "pitch": -0.06045535036917116,
+          "rotation": 0,
+          "target": "0-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0432824591032546,
+          "pitch": 0.33960923890638206,
+          "title": "Tilly The Studio Dog",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3067374020079292,
+          "pitch": 0.14403149280167682,
+          "title": "Editing Station Mobile",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.8361629913701876,
+          "pitch": -0.14580080936209505,
+          "title": "VP Monitor / Teleprompter",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.409336824638782,
+          "pitch": -0.045040889420413066,
+          "title": "Corner Voiceover Booth",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9141668871733026,
+          "pitch": 0.5279999234700465,
+          "title": "SandBags",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.123979418983744,
+          "pitch": 0.20527340497088176,
+          "title": "C-Stands",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8305562747792976,
+          "pitch": -0.3953664208660932,
+          "title": "Ladder",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.04571422990231788,
+          "pitch": -0.7300356410842515,
+          "title": "LED Lighting Grid",
+          "text": "Equipped with GVM LED Panels"
+        }
+      ]
+    },
+    {
+      "id": "3-edit-suite",
+      "name": "Edit Suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.386187739690669,
+          "pitch": 0.1340821544949442,
+          "rotation": 0,
+          "target": "1-main-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8430189311437157,
+          "pitch": 0.2876811572171061,
+          "title": "Virtual Production Station",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7005727315543773,
+          "pitch": 0.22939990943922695,
+          "title": "Editing Station",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.2674508575285053,
+          "pitch": 0.0840211892340168,
+          "title": "Editing Station",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4608915739290449,
+          "pitch": 0.16953781610741459,
+          "title": "Animation Station",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7231073542896578,
+          "pitch": 0.28314432394849476,
+          "title": "Animation Station",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Spectrum Studios",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
